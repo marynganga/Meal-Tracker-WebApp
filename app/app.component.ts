020d038acafdad42142252113029f499d9e1bb45
marynganga <marynganga.mw@gmail.com>
2017-09-17 12:47:13 +0300

    Add an about section.

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,8 +9,17 @@ import { Meal } from './meal.model';
 			<h1><img src="../resources/images/logo4.png" alt="Logo">Lisho Meal Tracker</h1>
 		</header>
 		<main>
-			<section  class="main">
-				
+			<section>
+			<div class="about">
+				<h3>About</h3>
+				<p>Lisho Meal Tracker is a simple yet elegant website application that lets you log your meals for the day.<br />
+				<ul>
+					<li>Enter the name of your meal, details and calories consumed and they will all be listed.</li>
+					<li>You can also edit your entries if need arises.</li>
+					<li>Another great feature is the calorie filter, which allows you to view meals according to whether there caloric content is above or below 500.</li>
+				</ul>
+				<p>We hope you enjoy our app as it helps you on your journey to a healthier lifestyle.</p>
+			</div>	
 			<!-- Call the tag for creating a new meal, with its custom event emitter -->
 				<new-meal (sendNewMeal)="addMeal($event)"></new-meal>
 			<!--Display the list of meals entered by the user, connect it to the list in appcomponent --> 
